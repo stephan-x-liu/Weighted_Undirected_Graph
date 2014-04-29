5fe4b7c4c02748b512a82e81dcea5fd6b7ac09a1
SebastianMerz <Sebastian@sebastianmerz.com>
2014-04-29 03:00:59 -0700

    Kruskal, ez pez 123

--- graphalg/Edge.java
+++ graphalg/Edge.java
@@ -1,12 +1,15 @@
+package graphalg;
 /**
  * Edge container object used to make tasks such as sorting the linked list easier.
- *
+ * 
+ * Note that this only works with undirected lists, since it assumes 2 edges with 
+ * opposite start points are equal.
  **/
 public class Edge implements Comparable{
 	protected Object vertex1;
 	protected Object vertex2;
 	protected int weight;
-	public Edge(Object v1, Objectx v2, int w){
+	public Edge(Object v1, Object v2, int w){
 		vertex1 = v1;
 		vertex2 = v2;
 		weight = w;
@@ -20,17 +23,34 @@ public class Edge implements Comparable{
 		return weight;
 	}
 
-	public int compareTo(Edge e){
-		if this.weight == e.weight(){
-			return 0;
-		}
-		if this.weight > e.weight(){
-			return 1;
-		}
-		if this.weight < e.weight(){
-			return -1;
-		}
+	public int compareTo(Object o){
+    if(o instanceof Edge){
+      Edge e = (Edge) o;
+      if(this.weight == e.weight()){
+        return 0;
+      }
+      if(this.weight > e.weight()){
+        return 1;
+      }
+      if(this.weight < e.weight()){
+        return -1;
+      }
+    }else{
+      return 0;
+    }
+    return 0;
 	}
+
+  public boolean equals(Object o){
+    if(o instanceof Edge){
+      Edge e = (Edge) o;
+      return (((vertex1 == e.vertex1 && vertex2 == e.vertex2) || 
+            (vertex1 == e.vertex2 && vertex2 == e.vertex1)) &&
+            weight == e.weight);
+    }else{
+      return false;
+    }
+  }
 
 	public int hashCode() {
     if (vertex1.equals(vertex2)) {
--- graphalg/Kruskal.java
+++ graphalg/Kruskal.java
@@ -4,6 +4,8 @@ package graphalg;
 
 import graph.*;
 import set.*;
+import list.*;
+import dict.*;
 
 /**
  * The Kruskal class contains the method minSpanTree(), which implements
@@ -23,11 +25,46 @@ public class Kruskal {
   public static WUGraph minSpanTree(WUGraph g) {
     WUGraph output = new WUGraph();
     Object[] origVertices = g.getVertices();
+    DList<Edge> edgeList = new DList();
+    HashTable<Edge,Integer> edgeTable = new HashTable(g.edgeCount());
+    HashTable<Object,Integer> vertexTable = new HashTable(origVertices.length);
 
+    int j=0; //We keep (:) loop for elegance, but we need a counter for later.
     for(Object item:origVertices){
-        output.addVertex(item);
+      vertexTable.insert(item, j);
+      j++;
+      output.addVertex(item);
+      Neighbors neighbors = g.getNeighbors(item);
+      int i;
+      for(i=0;i<neighbors.neighborList.length;i++){
+        //This is going to give us every edge twice. The alternative is fancy detection though.
+        //Specifically via a hashtable. If we need one anyway we'll make it here.
+        //Oh fuck it, lets do it.
+        Edge e = new Edge(item, neighbors.neighborList[i], neighbors.weightList[i]);
+
+        if(edgeTable.find(e) == null){
+          edgeList.insertFront(e);
+          edgeTable.insert(e, 1);
+        }
+      }
     }
 
-    DisjointSet linkedSet = new DisjointSets(origVertices.length)
+    edgeList.sort();
+
+    DisjointSets linkedSet = new DisjointSets(origVertices.length);
+
+    for(Edge edge:edgeList){
+      int v1Int = vertexTable.find(edge.vertex1).value();
+      int v2Int = vertexTable.find(edge.vertex2).value();
+
+      if(linkedSet.find(v1Int) != linkedSet.find(v2Int)){
+        //In this case, these two vertices are not connected yet.
+        linkedSet.union(v1Int, v2Int);
+        output.addEdge(edge.vertex1, edge.vertex2, edge.weight);
+      }
+    }
+
+    return output;
+  }
 
 }
